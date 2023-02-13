a541aba3b423f60e5844499d4af4f154b2219de4
nguyenngocbaocmt02 <nguyenngocbaocmt@gmail.com>
2023-02-13 11:26:47 +0000

    update

--- main_src/gp_cdpp_main.py
+++ main_src/gp_cdpp_main.py
@@ -116,12 +116,15 @@ if __name__ == "__main__":
     varthetas = [0.25, 0.5, 0, 0.75, 1.0]
     for vartheta in varthetas:
         for dataset in datasets:    
-            if dataset != "id_15" and vartheta != 0:
-                continue      
+            #if dataset != "id_11":
+            #    continue      
             dataset_path = os.path.join(os.path.join(datasets_folder_path, dataset))
             save_path = os.path.join(os.path.join(os.path.join(result_folder_path, "gp_cdpp_" + str(vartheta)), dataset), "result.csv")
             if os.path.exists(save_path):
                 continue
             dp, config = read_dataset(dataset_path)
             print(dataset, vartheta, config["snr"])  
-            gp_cdpp_solve_dataset(dp, config, vartheta, queries_num, save_path)
+            try:
+                gp_cdpp_solve_dataset(dp, config, vartheta, queries_num, save_path)
+            except:
+                pass
